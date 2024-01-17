8510fa709b419dc1a79eb3dc387ad8e8f9eff6b5
ingrah <147798703+ingrah@users.noreply.github.com>
2024-01-17 15:54:36 +0100

    update app.mjs

--- app.mjs
+++ app.mjs
@@ -1,1 +1,30 @@
 import chalk from "chalk";
+
+const myJoke = "Her burde det være en vits";
+
+
+//Hvordan skal jeg bruke chalk for å skrive ut denne med forskjellige farger på hvert ord?
+
+//Hvordan kan jeg splitte opp setningen?
+//Hvordan kan jeg skrive ut hvert ord? hvordan gjøre setningen om til ei liste, hvordan få skrevet ut.
+
+console.log(myJoke);
+
+const words = myJoke.split(" "); 
+
+let coloredmyJoke =""; 
+
+for (let i = 0; i < words.length; i++){
+    // choose a color for each words 
+    const color = chalk.rgb(getRandomColor(),getRandomColor(),getRandomColor());
+
+    //apply the color to the words 
+    coloredmyJoke += color(words[i]) + " ";
+}
+console.log(coloredmyJoke);
+
+// function to generate a random color component 
+
+function getRandomColor() {
+    return Math.floor(Math.random()* 256);
+}
